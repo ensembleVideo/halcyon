159dad8d4c905725dd686b67a44593fbb312ba8d
Jim Pease <jmpease@ensemblevideo.com>
2017-03-14 12:34:25 -0400

    Added custom contract resolver to ignore model properties containing HalEmbedded attribute.

---
+++ src/Halcyon/HAL/Json/JsonHalAttributesContractResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Halcyon.HAL.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Halcyon.HAL.Json
+{
+    public class JsonHalAttributesContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var properties = base.CreateProperties(type, memberSerialization);
+
+            // Ignore properties w/ HalEmbedded attribute
+            return
+                properties.Where(p => !p.AttributeProvider.GetAttributes(typeof(HalEmbeddedAttribute), true).Any())
+                    .ToList();
+        }
+    }
+}
